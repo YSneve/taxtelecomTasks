3fe91dd92a949d9eba8dc3e07a3e4e8fb0cf0216
YSneve <cerg.2002@bk.ru>
2023-03-13 19:01:07 +0500

    Задание 1

--- taxtelecomTasks/Program.cs
+++ taxtelecomTasks/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void main(String[] args)
+{
+    Console.WriteLine("Введите строку");
+    string inputStr = Console.ReadLine();
+
+    Console.WriteLine(firstTaskReverse(inputStr));
+}
+// Задание 1
+string firstTaskReverse(string userString)
+{
+
+    return userString.Length % 2 != 0 ? ReverseString(userString) + userString : oddCase(userString);
+
+    // Функция для нечетного кол-ва символов в строке
+    string oddCase(string toReverseStr)
+    {
+        string firstHalf = ReverseString(toReverseStr.Substring(0, toReverseStr.Length / 2)); // Развернутая первая половина входной строки
+        string secondHalf = ReverseString(toReverseStr.Substring(toReverseStr.Length / 2, toReverseStr.Length / 2)); // Развернутая вторая половина входной строки
+
+        return firstHalf + secondHalf;
+    }
+
+    //Функция разворота строки
+    string ReverseString(string toReverseStr)
+    {
+        var toReverseList = toReverseStr.ToCharArray();
+
+        Array.Reverse(toReverseList);
+
+        return new string(toReverseList);
+    }
+}
