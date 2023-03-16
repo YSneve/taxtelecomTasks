15056d1bf88f5799a31c309727cff8d9150e37aa
YSneve <cerg.2002@bk.ru>
2023-03-16 12:54:22 +0500

    Выполнено задание 3. Небольшие исправления старого кода

--- taxtelecomTasks/Program.cs
+++ taxtelecomTasks/Program.cs
@@ -1,23 +1,26 @@
 ﻿
 using System.Text.RegularExpressions;
 
-internal class program
+namespace taxtelecomTasks;
+
+internal class Program
 {
     static void Main(String[] args)
     {
 
         Console.WriteLine("Введите строку");
-        string inputStr = Console.ReadLine();
+        var inputStr = Console.ReadLine();
 
 
-        if (isAllLower(inputStr))
-            Console.WriteLine(firstTaskReverse(inputStr));
+        if (IsAllLower(inputStr))
+            Console.WriteLine("Результат: {0}", FirstTaskReverse(inputStr));
         else
-            Console.WriteLine("Ошибка! Введены не подходящие символы." +
-                "\nСледующие, из введенных символов, не являются подходящими: {0}",getErrors(inputStr));
+        {
+            WriteErrors(inputStr);
+        }
     }
     // Задание 1
-    static string firstTaskReverse(string userString)
+    private static string FirstTaskReverse(string userString)
     {
 
         return userString.Length % 2 != 0 ? ReverseString(userString) + userString : oddCase(userString);
@@ -25,8 +28,8 @@ internal class program
         // Функция для нечетного кол-ва символов в строке
         string oddCase(string toReverseStr)
         {
-            string firstHalf = ReverseString(toReverseStr.Substring(0, toReverseStr.Length / 2)); // Развернутая первая половина входной строки
-            string secondHalf = ReverseString(toReverseStr.Substring(toReverseStr.Length / 2, toReverseStr.Length / 2)); // Развернутая вторая половина входной строки
+            string firstHalf = ReverseString(toReverseStr[..(toReverseStr.Length / 2)]); // Развернутая первая половина входной строки
+            string secondHalf = ReverseString(toReverseStr[(toReverseStr.Length / 2)..]); // Развернутая вторая половина входной строки
 
             return firstHalf + secondHalf;
         }
@@ -43,29 +46,36 @@ internal class program
     }
 
     //Задание 2
-    static bool isAllLower(string checkString)
+    private static bool IsAllLower(string checkString)
     {
         //Регулярное выражение, проверяющее на то, что строка от начала и до конца
         //является набором символов от a до z 
-        var regExpr = @"(^[a-z]*$)";
-        
-        var lowLetExpr = new Regex(regExpr);
-       
+        var lowLetExpr = new Regex(@"(^[a-z]*$)");
+
         return lowLetExpr.IsMatch(checkString);
     }
-    static string getErrors(string inString)
+
+    //Задание 3
+    private static void WriteErrors(string inString)
     {
         //Регулярное выражение, проверяющее на то, что символ
         //Является символом из диапазона от a до z 
-        var regExpr = @"([a-z]*)";
-        var lowerLettExpr = new Regex(regExpr);
-        
+        var lowerLetterExpr = new Regex(@"([a-z]*)");
+
         //Удаление подходящих для ввода символов
         //Удаление повторяющихся неподходящих для вывода символов
         //Возвращение строки из не повторяющихся, не подходящих для ввода символов
-        return string
-            .Join("", new HashSet<char>(lowerLettExpr.
-            Replace(inString, "").
-            ToCharArray()));          
+        var wrongLettersSet = new HashSet<char>(lowerLetterExpr.
+                Replace(inString, "").
+                ToCharArray());
+
+        Console.WriteLine("Ошибка! Введены не подходящие символы." +
+                          "\nСледующие, из введенных символов, не являются подходящими:" +
+                          "\nСимвол; Кол-во повторений");
+        foreach (var wrongLetter in wrongLettersSet)
+        {
+            var matchCount = new Regex(wrongLetter.ToString()).Matches(inString).Count;
+            Console.WriteLine("{0} : {1}", wrongLetter, matchCount);
+        }
     }
 }
